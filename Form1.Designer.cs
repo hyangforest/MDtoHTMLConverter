b1ff3bc64e98343dce31ebe289a8c71c41559546
hyangforest <hyangforest@gmail.com>
2023-10-02 01:12:57 +0900

    init

---
+++ Form1.Designer.cs
@@ -0,0 +1,236 @@
+﻿namespace MDtoHTMLConverter
+{
+    partial class formMDtoHTML
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(formMDtoHTML));
+            this.menuBar = new System.Windows.Forms.MenuStrip();
+            this.programToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.initToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tistoryToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.infoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pnlMain = new System.Windows.Forms.TableLayoutPanel();
+            this.pnlMarkdown = new System.Windows.Forms.Panel();
+            this.btmMd = new System.Windows.Forms.Button();
+            this.btnWrtie = new System.Windows.Forms.Button();
+            this.pnlFooter = new System.Windows.Forms.Panel();
+            this.pnlPreview = new System.Windows.Forms.Panel();
+            this.btnHTML = new System.Windows.Forms.Button();
+            this.menuBar.SuspendLayout();
+            this.pnlMain.SuspendLayout();
+            this.pnlMarkdown.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuBar
+            // 
+            this.menuBar.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuBar.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.programToolStripMenuItem});
+            this.menuBar.Location = new System.Drawing.Point(0, 0);
+            this.menuBar.Name = "menuBar";
+            this.menuBar.Size = new System.Drawing.Size(434, 28);
+            this.menuBar.TabIndex = 1;
+            this.menuBar.Text = "menuStrip1";
+            // 
+            // programToolStripMenuItem
+            // 
+            this.programToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.initToolStripMenuItem,
+            this.tistoryToolStripMenuItem,
+            this.infoToolStripMenuItem});
+            this.programToolStripMenuItem.Name = "programToolStripMenuItem";
+            this.programToolStripMenuItem.Size = new System.Drawing.Size(83, 24);
+            this.programToolStripMenuItem.Text = "프로그램";
+            // 
+            // initToolStripMenuItem
+            // 
+            this.initToolStripMenuItem.Name = "initToolStripMenuItem";
+            this.initToolStripMenuItem.Size = new System.Drawing.Size(137, 26);
+            this.initToolStripMenuItem.Text = "초기화";
+            this.initToolStripMenuItem.Click += new System.EventHandler(this.initToolStripMenuItem_Click);
+            // 
+            // tistoryToolStripMenuItem
+            // 
+            this.tistoryToolStripMenuItem.Name = "tistoryToolStripMenuItem";
+            this.tistoryToolStripMenuItem.Size = new System.Drawing.Size(137, 26);
+            this.tistoryToolStripMenuItem.Text = "Tistory";
+            this.tistoryToolStripMenuItem.Click += new System.EventHandler(this.tistoryToolStripMenuItem_Click);
+            // 
+            // infoToolStripMenuItem
+            // 
+            this.infoToolStripMenuItem.Name = "infoToolStripMenuItem";
+            this.infoToolStripMenuItem.Size = new System.Drawing.Size(137, 26);
+            this.infoToolStripMenuItem.Text = "문의";
+            this.infoToolStripMenuItem.Click += new System.EventHandler(this.infoToolStripMenuItem_Click);
+            // 
+            // pnlMain
+            // 
+            this.pnlMain.ColumnCount = 1;
+            this.pnlMain.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.pnlMain.Controls.Add(this.pnlMarkdown, 0, 0);
+            this.pnlMain.Controls.Add(this.pnlFooter, 0, 3);
+            this.pnlMain.Controls.Add(this.pnlPreview, 0, 1);
+            this.pnlMain.Controls.Add(this.btnHTML, 0, 2);
+            this.pnlMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlMain.Location = new System.Drawing.Point(0, 28);
+            this.pnlMain.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pnlMain.Name = "pnlMain";
+            this.pnlMain.RowCount = 4;
+            this.pnlMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 8F));
+            this.pnlMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 78F));
+            this.pnlMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 8F));
+            this.pnlMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 6F));
+            this.pnlMain.Size = new System.Drawing.Size(434, 973);
+            this.pnlMain.TabIndex = 2;
+            // 
+            // pnlMarkdown
+            // 
+            this.pnlMarkdown.Controls.Add(this.btmMd);
+            this.pnlMarkdown.Controls.Add(this.btnWrtie);
+            this.pnlMarkdown.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlMarkdown.Location = new System.Drawing.Point(30, 15);
+            this.pnlMarkdown.Margin = new System.Windows.Forms.Padding(30, 15, 30, 15);
+            this.pnlMarkdown.Name = "pnlMarkdown";
+            this.pnlMarkdown.Size = new System.Drawing.Size(374, 47);
+            this.pnlMarkdown.TabIndex = 4;
+            // 
+            // btmMd
+            // 
+            this.btmMd.Dock = System.Windows.Forms.DockStyle.Left;
+            this.btmMd.Font = new System.Drawing.Font("맑은 고딕", 9F);
+            this.btmMd.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(130)))), ((int)(((byte)(130)))), ((int)(((byte)(130)))));
+            this.btmMd.Image = ((System.Drawing.Image)(resources.GetObject("btmMd.Image")));
+            this.btmMd.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btmMd.Location = new System.Drawing.Point(0, 0);
+            this.btmMd.Margin = new System.Windows.Forms.Padding(30, 15, 30, 15);
+            this.btmMd.Name = "btmMd";
+            this.btmMd.Padding = new System.Windows.Forms.Padding(20, 5, 0, 5);
+            this.btmMd.Size = new System.Drawing.Size(185, 47);
+            this.btmMd.TabIndex = 3;
+            this.btmMd.Text = "MD 파일선택";
+            this.btmMd.UseVisualStyleBackColor = true;
+            this.btmMd.Click += new System.EventHandler(this.btmMd_Click);
+            // 
+            // btnWrtie
+            // 
+            this.btnWrtie.Dock = System.Windows.Forms.DockStyle.Right;
+            this.btnWrtie.Font = new System.Drawing.Font("맑은 고딕", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btnWrtie.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(130)))), ((int)(((byte)(130)))), ((int)(((byte)(130)))));
+            this.btnWrtie.Image = ((System.Drawing.Image)(resources.GetObject("btnWrtie.Image")));
+            this.btnWrtie.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnWrtie.Location = new System.Drawing.Point(189, 0);
+            this.btnWrtie.Name = "btnWrtie";
+            this.btnWrtie.Padding = new System.Windows.Forms.Padding(20, 5, 0, 5);
+            this.btnWrtie.Size = new System.Drawing.Size(185, 47);
+            this.btnWrtie.TabIndex = 0;
+            this.btnWrtie.Text = "내용 작성하기";
+            this.btnWrtie.UseVisualStyleBackColor = true;
+            this.btnWrtie.Click += new System.EventHandler(this.btnWrtie_Click);
+            // 
+            // pnlFooter
+            // 
+            this.pnlFooter.BackColor = System.Drawing.Color.White;
+            this.pnlFooter.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pnlFooter.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlFooter.ForeColor = System.Drawing.SystemColors.Control;
+            this.pnlFooter.Location = new System.Drawing.Point(15, 914);
+            this.pnlFooter.Margin = new System.Windows.Forms.Padding(15, 2, 15, 2);
+            this.pnlFooter.Name = "pnlFooter";
+            this.pnlFooter.Size = new System.Drawing.Size(404, 57);
+            this.pnlFooter.TabIndex = 0;
+            this.pnlFooter.Paint += new System.Windows.Forms.PaintEventHandler(this.pnlFooter_Paint);
+            // 
+            // pnlPreview
+            // 
+            this.pnlPreview.AllowDrop = true;
+            this.pnlPreview.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.pnlPreview.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlPreview.Location = new System.Drawing.Point(30, 77);
+            this.pnlPreview.Margin = new System.Windows.Forms.Padding(30, 0, 30, 0);
+            this.pnlPreview.Name = "pnlPreview";
+            this.pnlPreview.Size = new System.Drawing.Size(374, 758);
+            this.pnlPreview.TabIndex = 0;
+            this.pnlPreview.DragDrop += new System.Windows.Forms.DragEventHandler(this.pnlPreview_DragDrop);
+            this.pnlPreview.DragEnter += new System.Windows.Forms.DragEventHandler(this.pnlPreview_DragEnter);
+            // 
+            // btnHTML
+            // 
+            this.btnHTML.BackColor = System.Drawing.Color.Transparent;
+            this.btnHTML.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnHTML.Font = new System.Drawing.Font("맑은 고딕", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btnHTML.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(130)))), ((int)(((byte)(130)))), ((int)(((byte)(130)))));
+            this.btnHTML.Image = ((System.Drawing.Image)(resources.GetObject("btnHTML.Image")));
+            this.btnHTML.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnHTML.Location = new System.Drawing.Point(100, 850);
+            this.btnHTML.Margin = new System.Windows.Forms.Padding(100, 15, 95, 15);
+            this.btnHTML.Name = "btnHTML";
+            this.btnHTML.Padding = new System.Windows.Forms.Padding(20, 5, 0, 5);
+            this.btnHTML.Size = new System.Drawing.Size(239, 47);
+            this.btnHTML.TabIndex = 3;
+            this.btnHTML.Text = "HTML 파일로 변환하기";
+            this.btnHTML.UseVisualStyleBackColor = false;
+            this.btnHTML.Click += new System.EventHandler(this.btnHTML_Click);
+            // 
+            // formMDtoHTML
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(434, 1001);
+            this.Controls.Add(this.pnlMain);
+            this.Controls.Add(this.menuBar);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuBar;
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "formMDtoHTML";
+            this.Text = "마크다운 HTML 파일 생성하기";
+            this.menuBar.ResumeLayout(false);
+            this.menuBar.PerformLayout();
+            this.pnlMain.ResumeLayout(false);
+            this.pnlMarkdown.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.MenuStrip menuBar;
+        private System.Windows.Forms.ToolStripMenuItem programToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem initToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem tistoryToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem infoToolStripMenuItem;
+        private System.Windows.Forms.TableLayoutPanel pnlMain;
+        private System.Windows.Forms.Panel pnlFooter;
+        private System.Windows.Forms.Button btmMd;
+        private System.Windows.Forms.Panel pnlPreview;
+        private System.Windows.Forms.Button btnHTML;
+        private System.Windows.Forms.Button btnWrtie;
+        private System.Windows.Forms.Panel pnlMarkdown;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,384 @@
+﻿using MarkdownSharp;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+//using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
+//using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace MDtoHTMLConverter
+{
+    public partial class formMDtoHTML : Form
+    {
+        string fileName, filePath = string.Empty;
+        bool isInitState, isErrorText = true;
+        TextBox textBox;
+        enum ProgramSatatus 
+        {
+            INIT,
+            FILE_READ,
+            FILE_SAVE,
+            FINISH
+        }
+
+        public formMDtoHTML()
+        {
+            InitializeComponent();
+            InitProgram();
+            LayoutFooter();
+        }
+        #region Method
+        /// <summary>
+        /// 프로그램 초기화
+        /// 1. 미리보기 panel 기본 스타일 지정
+        /// 2. 파일 경로 empty
+        /// </summary>
+        private void InitProgram()
+        {
+            // 상태 초기화
+            this.isInitState = true;
+            this.isErrorText = false;
+
+            // panel
+            this.pnlPreview.Controls.Clear();
+            this.pnlPreview.BackColor = Color.WhiteSmoke;
+
+            // 미리보기 default style
+            Label lblPreview = new Label();
+            lblPreview.Text = "마크다운 파일을 \n선택 또는 드래그하여\n업로드 해주세요.";
+            lblPreview.Width = 150;
+            lblPreview.Height = 70;
+            lblPreview.AutoSize = true;
+            lblPreview.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            lblPreview.Font = new Font("맑은 고딕", 11, FontStyle.Bold);
+            lblPreview.ForeColor = Color.Gray;
+
+            this.pnlPreview.Controls.Add(lblPreview);
+
+            // 안내문구 레이아웃 정렬
+            int labelX = (this.pnlPreview.Width - lblPreview.Width) / 2;
+            int labelY = (this.pnlPreview.Height - lblPreview.Height) / 2;
+            lblPreview.Location = new Point(labelX, labelY);
+
+            // 파일경로 리셋
+            this.filePath = string.Empty;
+            this.fileName = string.Empty;
+        }
+
+        private string GetMessageBoxTitle(ProgramSatatus status)
+        {
+            switch (status)
+            {
+                case ProgramSatatus.INIT:
+                    return "프로그램";
+                case ProgramSatatus.FILE_READ:
+                    return "파일선택";
+                case ProgramSatatus.FILE_SAVE:
+                    return "파일변환";
+                case ProgramSatatus.FINISH:
+                    return "파일저장";
+                default:
+                    return string.Empty;
+            }
+        }
+
+        /// <summary>
+        /// 마크다운 파일 내용 가져오기
+        /// 정상/오류 : 파일/오류 내용
+        /// 파일 변환 오류 제어 메시지를 위한 플래그 지정
+        /// </summary>
+        /// <returns></returns>
+        private string ReadMarkdownContent()
+        {
+            string result = string.Empty;
+
+            try
+            {
+                using (StreamReader reader = new StreamReader(filePath))
+                {
+                    result = reader.ReadToEnd();
+                }
+            }
+            catch (Exception ex)
+            {
+                isErrorText = true;
+                result = ex.Message;
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// 마크다운 파일 내용 확인 및 작성 모드 스타일 변경
+        /// </summary>
+        /// <param name="isReadOnly"></param>
+        private void CreateTextMode(bool isReadOnly)
+        {
+            textBox = new TextBox();
+            textBox.Multiline = true; // 여러 줄의 텍스트 허용
+            textBox.ScrollBars = ScrollBars.Vertical; // 수직 스크롤바 사용
+            textBox.Dock = DockStyle.Fill; // 부모 컨테이너 크기에 맞게 조정
+            textBox.ReadOnly = isReadOnly;
+            textBox.BackColor = Color.Black;
+            textBox.Text = isReadOnly ? ReadMarkdownContent() : string.Empty;
+            textBox.Font = new Font("Arial", 12, FontStyle.Bold); // Arial 글꼴, 크기 12, Bold 스타일
+            textBox.ForeColor = Color.White;
+            textBox.Padding = new System.Windows.Forms.Padding(10);
+
+            this.pnlPreview.Controls.Clear();
+            this.pnlPreview.Controls.Add(textBox);
+            this.pnlPreview.BackColor = Color.Black;
+        }
+
+        /// <summary>
+        /// 웹 브라우저 및 메일 기본앱 실행 
+        /// </summary>
+        /// <param name="url"></param>
+        private void OpenBrowser(string url)
+        {
+            System.Diagnostics.Process.Start(url);
+        }
+
+        /// <summary>
+        /// 푸터 배치
+        /// </summary>
+        private void LayoutFooter()
+        {
+            Label lblHyangforest = new Label();
+            lblHyangforest.Text = "made by hyangforest.";
+            lblHyangforest.Width = 120;
+            lblHyangforest.Height = 20;
+            lblHyangforest.AutoSize = true;
+            lblHyangforest.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            lblHyangforest.Font = new Font("Consolas", 8, FontStyle.Regular);
+            lblHyangforest.ForeColor = Color.FromArgb(255, 130, 130, 130);
+
+            int labelX = (this.pnlFooter.Width - lblHyangforest.Width) / 2;
+            int labelY = (this.pnlFooter.Height - lblHyangforest.Height) / 2;
+            lblHyangforest.Location = new Point(labelX, labelY);
+
+            this.pnlFooter.Controls.Add(lblHyangforest);
+        }
+
+       
+        private DialogResult ConfirmMessageBox(string title, string message) 
+        {
+            DialogResult result = MessageBox.Show(title, message, MessageBoxButtons.OKCancel);
+            return result;
+        }
+
+        private void AlertMessageBox(string title, string message)
+        {
+            MessageBox.Show(message, title);
+        }
+        #endregion
+
+        #region Event
+        /// <summary>
+        /// 푸터 위쪽 테두리 그리기
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void pnlFooter_Paint(object sender, PaintEventArgs e)
+        {
+            int lineWidth = pnlFooter.Width; // 패널 너비만큼
+            Color customColor = Color.FromArgb(255, 220, 220, 220);
+
+            Pen linePen = new Pen(customColor, 1); // 색상과 선 두께 설정
+
+            e.Graphics.DrawLine(linePen, 0, 0, lineWidth, 0);
+        }
+        /// <summary>
+        /// Markdown 파일 업로드
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btmMd_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            openFileDialog.InitialDirectory = "C:\\"; // 초기 디렉토리 설정
+            openFileDialog.Filter = "마크다운 파일(*.md)|*.md|모든 파일 (*.*)|*.*"; // 파일 필터 설정
+            openFileDialog.Multiselect = false; // 단일 파일 선택
+            openFileDialog.Title = "마크다운(*.md) 파일 가져오기";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                isInitState = false;
+
+                // file 읽어오기
+                filePath = openFileDialog.FileName;
+
+                // 확장자를 제외한 파일명 가져오기
+                fileName = Path.GetFileNameWithoutExtension(filePath);
+
+                // 텍스트 컨트롤 생성(파일 내용 가져오기 및 작성하기)
+                CreateTextMode(true);
+            }
+        }
+
+        /// <summary>
+        /// 내용 작성하기
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnWrtie_Click(object sender, EventArgs e)
+        {
+            isInitState = false;
+            filePath = string.Empty;
+            fileName = string.Format("newMarkdown_{0}", DateTime.Now.ToString("yyyyMMddHHmmss"));
+            CreateTextMode(false);
+        }
+
+        private void initToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DialogResult result = this.ConfirmMessageBox("초기 상태로 되돌리시겠습니까?", GetMessageBoxTitle(ProgramSatatus.INIT));
+
+            if (result == DialogResult.OK)
+            {
+                this.InitProgram();
+            }
+        }
+
+        /// <summary>
+        /// 메뉴 : Tistory
+        /// 프로그램 개발 스토리 링크 열기
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void tistoryToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.OpenBrowser("https://hyangforest.tistory.com/");
+        }
+
+        /// <summary>
+        /// 메뉴 : 문의
+        /// 프로그램 오류 및 의견 이메일 발송
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void infoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.OpenBrowser("mailto:hyangforest@gami.com?subject=프로그램 문의");
+        }
+
+        private void pnlPreview_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.Copy;
+            }
+            else
+            {
+                e.Effect = DragDropEffects.None;
+            }
+        }
+
+        private void pnlPreview_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+
+            if (files.Length == 1)
+            {
+                // 파일 가져오기
+                filePath = files[0];
+
+                if (Path.GetExtension(filePath) == ".md")
+                {
+                    // 확장자를 제외한 파일명 가져오기
+                    fileName = Path.GetFileNameWithoutExtension(filePath);
+
+                    // 텍스트 컨트롤 생성(파일 내용 가져오기 및 작성하기)
+                    CreateTextMode(true);
+                }
+                else
+                {
+                    this.AlertMessageBox("확장자 .md만 가져올 수 있습니다.", GetMessageBoxTitle(ProgramSatatus.FILE_READ));
+                }
+            }
+            else
+            {
+                this.AlertMessageBox("하나의 파일만 가져올 수 있습니다.", GetMessageBoxTitle(ProgramSatatus.FILE_READ));
+            }
+
+
+        }
+
+        private void btnHTML_Click(object sender, EventArgs e)
+        {
+            if (isInitState)
+            {
+                this.AlertMessageBox("변환할 내용이 없습니다.", GetMessageBoxTitle(ProgramSatatus.FILE_SAVE));
+
+                return;
+            }
+
+            if (isErrorText)
+            {
+                this.AlertMessageBox("파일 변환이 실패하였습니다.", GetMessageBoxTitle(ProgramSatatus.FILE_SAVE));
+                return;
+            }
+
+
+            FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
+            folderBrowserDialog.Description = "저장 폴더 선택";
+            folderBrowserDialog.SelectedPath = @"C:\";
+
+            DialogResult result = folderBrowserDialog.ShowDialog();
+
+            if (result == DialogResult.OK)
+            {
+                // 사용자가 선택한 디렉토리의 경로
+                string selectedFolder = folderBrowserDialog.SelectedPath;
+
+                // Markdown을 HTML로 변환
+                Markdown markdown = new Markdown();
+                string html = markdown.Transform(textBox.Text);
+
+                // HTML 파일로 저장
+                string htmlFilePath = string.Format("{0}/{1}.html", selectedFolder, fileName);
+
+                bool isConvert = true;
+
+                if (File.Exists(htmlFilePath))
+                {
+                    DialogResult fileUpdate = this.ConfirmMessageBox("이미 동일한 파일명이 존재합니다. 파일을 파일을 변환하시겠습니까?", GetMessageBoxTitle(ProgramSatatus.FILE_SAVE));
+
+                    if (fileUpdate == DialogResult.Cancel)
+                    {
+                        isConvert = false;
+                    }
+                }
+
+                if (isConvert)
+                {
+                    File.WriteAllText(htmlFilePath, html);
+
+                    DialogResult openBroswer = this.ConfirmMessageBox("변환이 완료되었습니다. 웹 페이지를 실행하시겠습니까?", GetMessageBoxTitle(ProgramSatatus.FINISH));
+
+                    if (openBroswer == DialogResult.OK)
+                    {
+                        this.OpenBrowser(htmlFilePath);
+                    }
+                    else
+                    {
+                        this.AlertMessageBox("변환이 완료되었습니다.", GetMessageBoxTitle(ProgramSatatus.FINISH));
+                    }
+                }
+                else
+                {
+                    this.AlertMessageBox("파일명 수정 후 다시 시도해주십시오.", GetMessageBoxTitle(ProgramSatatus.FILE_SAVE));
+                }
+            }
+        }
+        #endregion
+    }
+}
